9f65dd2b58adc142ed4cef2a03856a39602628a5
chuongtang <chuong.tang@aol.com>
2021-11-13 15:55:29 -0700

    replaced NFT-mining with deploying code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,42 +2,37 @@ const colors = require('colors');
 
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-  const gameContract = await gameContractFactory.deploy(                     
+  const gameContract = await gameContractFactory.deploy(
     ["TangXuanZang", "SunWuKong", "ZhuBajie", "ShaWuJing"],       // Names
     ["https://chuongtang.github.io/sourceStore/CharImg/TangXuanZang.png", // Images
       "https://chuongtang.github.io/sourceStore/CharImg/SunWuKong.png",
       "https://chuongtang.github.io/sourceStore/CharImg/ZhuBaJie.png",
       "https://chuongtang.github.io/sourceStore/CharImg/ShaWuJing.png"],
-      [50, 300, 200, 100],                    // HP values
-      [25, 100, 100, 50],   // Attack damage values
-      "DemonLord",
-      "https://chuongtang.github.io/sourceStore/CharImg/Demon.png",
-      10000,
-      50                  
+    [50, 300, 200, 100],                    // HP values
+    [25, 100, 100, 50],   // Attack damage values
+    "DemonLord",
+    "https://chuongtang.github.io/sourceStore/CharImg/Demon.png",
+    10000,
+    50
   );
   await gameContract.deployed();
-  console.log("Contract deployed to:".bgGreen, gameContract.address);
+  console.log("Contract deployed to:".green, gameContract.address.yellow);
 
-  
   let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1".bgMagenta);
-
   txn = await gameContract.mintCharacterNFT(1);
   await txn.wait();
-  console.log("Minted NFT #2".bgRed);
 
-  txn = await gameContract.mintCharacterNFT(2);
+  txn = await gameContract.attackDemon();
   await txn.wait();
-  console.log("Minted NFT #3".bgCyan);
-
-  txn = await gameContract.mintCharacterNFT(3);
+  
+  txn = await gameContract.attackDemon();
   await txn.wait();
-  console.log("Minted NFT #4".bgYellow);
 
-  console.log("Done deploying and minting!".red);
-
+  console.log("Done attacking!".bgYellow)
+  /*"tokenURI" is the function inherited from ERC721*/
+  // // Get the value of the NFT's URI.
+  // let returnedTokenUri = await gameContract.tokenURI(1);
+  // console.log("Token URI:".bgBlue, returnedTokenUri);
 };
 
 const runMain = async () => {
@@ -45,7 +40,7 @@ const runMain = async () => {
     await main();
     process.exit(0);
   } catch (error) {
-    console.log(error.bgRed);
+    console.log(error);
     process.exit(1);
   }
 };
